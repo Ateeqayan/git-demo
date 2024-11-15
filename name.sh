3561a884a17337a131e5f99071e0daaf4fd53b97
root <root@DESKTOP-TDSD262.localdomain>
2024-11-15 07:19:39 +0000

    Modifed name.sh file

--- name.sh
+++ name.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+echo "********Code started*********"
 read -p "Enter Your Username: " Username
 read -p "Enter Your Password: " Password
 echo "Your username is $Username"
 echo "Your password is $Password"
+echo "********Code Ended**********"
